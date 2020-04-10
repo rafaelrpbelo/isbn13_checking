1007c36b349dcd2c31543da0042adb98b776870e
Rafael Belo <rafaelrpbelo@gmail.com>
2020-04-10 12:51:31 -0300

    Refactor algorithm
    Split ISBN13.check into smaller methods.

--- lib/isbn13.rb
+++ lib/isbn13.rb
@@ -1,25 +1,42 @@
 class ISBN13
-  def self.check(number)
-    multiplier = 1
+  PRIMARY_MULTIPLIER    = 1
+  SECONDARY_MULTIPLIER  = 3
 
-    # Take each digit, from left to right and multiply them alternatively by 1 and 3
-    sum = number.each_char.reduce(0) do |acc, char, a|
+  def self.check(number)
+    # Append the digit to number
+    "#{number}#{get_end_digit(number)}"
+  end
+
+  private
+
+  # Take each digit, from left to right and multiply them alternatively by 1 and 3
+  # and sum those numbers
+  def self.sum_digits_with_multiplier(number)
+    multiplier = PRIMARY_MULTIPLIER
+
+    number.each_char.reduce(0) do |acc, char, a|
       n = Integer(char)
 
-      # Sum those numbers
       acc       += n * multiplier
-      multiplier = multiplier == 1 ? 3 : 1
+      multiplier = alternate_multiplier(multiplier)
 
       acc
     end
+  end
+
+  def self.alternate_multiplier(current_multiplier)
+    current_multiplier == PRIMARY_MULTIPLIER ?
+      SECONDARY_MULTIPLIER :
+      PRIMARY_MULTIPLIER
+  end
+
+  def self.get_end_digit(number)
+    sum = sum_digits_with_multiplier(number)
 
     # Take mod 10 of the summed figure
     digit = sum % 10
 
     # Subtract 10 and if the end number is 10, make it 0
-    digit = 10 - digit
-
-    # Append the digit to number
-    "#{number}#{digit}"
+    10 - digit
   end
 end
